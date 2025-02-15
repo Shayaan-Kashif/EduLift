4e64404f6e0e6a3c054825bebbedcf1749cc395d
Shayaan Kashif <shayaankashif27@gmail.com>
2025-02-15 15:38:25 -0500

    Added buttons to upload or take a picture

--- lib/markings.dart
+++ lib/markings.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class Markings extends StatefulWidget {
   const Markings({super.key});
@@ -8,8 +10,51 @@ class Markings extends StatefulWidget {
 }
 
 class _MarkingsState extends State<Markings> {
-
   String selectedButton = '';
+  File? _image1;
+  File? _image2;
+
+  Future<void> _pickImage(int imageNumber) async {
+    final picker = ImagePicker();
+
+    // Show the option to take a photo or choose from gallery
+    final pickedFile = await showModalBottomSheet<XFile>(
+      context: context,
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ListTile(
+                leading: Icon(Icons.camera),
+                title: Text('Take a photo'),
+                onTap: () async {
+                  Navigator.pop(context, await picker.pickImage(source: ImageSource.camera));
+                },
+              ),
+              ListTile(
+                leading: Icon(Icons.photo_album),
+                title: Text('Choose from gallery'),
+                onTap: () async {
+                  Navigator.pop(context, await picker.pickImage(source: ImageSource.gallery));
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+
+    if (pickedFile != null) {
+      setState(() {
+        if (imageNumber == 1) {
+          _image1 = File(pickedFile.path);
+        } else if (imageNumber == 2) {
+          _image2 = File(pickedFile.path);
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,10 +79,10 @@ class _MarkingsState extends State<Markings> {
                     selectedButton = 'MCQS';
                   });
                 },
-                splashColor: Colors.transparent,  // Disable the splash (ripple) effect
-                highlightColor: Colors.transparent, // Disable the highlight effect
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
                 child: Container(
-                  margin: EdgeInsets.only(top: 20.0, left: 20), // Margin at the top
+                  margin: EdgeInsets.only(top: 20.0, left: 20),
                   padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
                   decoration: BoxDecoration(
                     color: selectedButton == 'MCQS' ? Colors.grey : Colors.white,
@@ -60,7 +105,6 @@ class _MarkingsState extends State<Markings> {
                 ),
               ),
 
-
               // Second button (Option 2) with rounded corners and shadow
               InkWell(
                 onTap: () {
@@ -69,10 +113,10 @@ class _MarkingsState extends State<Markings> {
                     selectedButton = 'Long Answers';
                   });
                 },
-                splashColor: Colors.transparent,  // Disable the splash (ripple) effect
-                highlightColor: Colors.transparent, // Disable the highlight effect
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
                 child: Container(
-                  margin: EdgeInsets.only(top: 20.0, left: 20), // Margin at the top
+                  margin: EdgeInsets.only(top: 20.0, left: 20),
                   padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
                   decoration: BoxDecoration(
                     color: selectedButton == 'Long Answers' ? Colors.grey : Colors.white,
@@ -93,9 +137,83 @@ class _MarkingsState extends State<Markings> {
                     ),
                   ),
                 ),
-              )
+              ),
             ],
           ),
+
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              InkWell(
+                onTap: () {
+                  _pickImage(1);
+                },
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                child: Container(
+                  margin: EdgeInsets.only(top: 50.0, left: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        offset: Offset(4, 4),
+                        blurRadius: 8,
+                      ),
+                    ],
+                  ),
+                  child: Text(
+                    "Pick Image 1",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+
+
+              InkWell(
+                onTap: () {
+                  _pickImage(2);
+                },
+                splashColor: Colors.transparent,
+                highlightColor: Colors.transparent,
+                child: Container(
+                  margin: EdgeInsets.only(top: 50.0, left: 20),
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        offset: Offset(4, 4),
+                        blurRadius: 8,
+                      ),
+                    ],
+                  ),
+                  child: Text(
+                    "Pick Image 2",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          // Display images if selected
+          if (_image1 != null)
+            Image.file(_image1!),
+          if (_image2 != null)
+            Image.file(_image2!),
         ],
       ),
     );
