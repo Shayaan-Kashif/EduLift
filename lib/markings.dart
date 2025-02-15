2dc669ae0470f93bd0b476843fbd7c828d639a0b
Shayaan Kashif <shayaankashif27@gmail.com>
2025-02-15 10:03:30 -0500

    Made the markings widget stateful and added functionality to detect a button click

--- lib/markings.dart
+++ lib/markings.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-class markings extends StatelessWidget {
-  const markings({super.key});
+class Markings extends StatefulWidget {
+  const Markings({super.key});
+
+  @override
+  _MarkingsState createState() => _MarkingsState();
+}
+
+class _MarkingsState extends State<Markings> {
 
   @override
   Widget build(BuildContext context) {
@@ -19,58 +25,65 @@ class markings extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               // First button (Option 1) with rounded corners and shadow
-              Container(
-                margin: EdgeInsets.only(top: 20.0,left: 20), // Margin at the top
-                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      offset: Offset(4, 4),
-                      blurRadius: 8,
+              InkWell(
+                onTap: () {
+                  print('MCQS button tapped!');
+                },
+                child: Container(
+                  margin: EdgeInsets.only(top: 20.0, left: 20), // Margin at the top
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        offset: Offset(4, 4),
+                        blurRadius: 8,
+                      ),
+                    ],
+                  ),
+                  child: Text(
+                    "MCQS",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
-                child: Text(
-                  "MCQS",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-              SizedBox(width: 10), // Space between buttons
+
 
               // Second button (Option 2) with rounded corners and shadow
-              Container(
-                margin: EdgeInsets.only(top: 20.0), // Margin at the top
-                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black.withOpacity(0.2),
-                      offset: Offset(4, 4),
-                      blurRadius: 8,
+              InkWell(
+                onTap: () {
+                  print('Long Answers button tapped!');
+                },
+                child: Container(
+                  margin: EdgeInsets.only(top: 20.0, left: 20), // Margin at the top
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        offset: Offset(4, 4),
+                        blurRadius: 8,
+                      ),
+                    ],
+                  ),
+                  child: Text(
+                    "Long Answers",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
                     ),
-                  ],
-                ),
-                child: Text(
-                  "Long Answers",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
+              )
             ],
           ),
-
-
         ],
       ),
     );
